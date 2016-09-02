20e15085255ed1471454c158244e6cc382aa748e
Tyler Constance <tyler@tylerconstance.com>
2016-09-02 18:02:26 -0400

    Finished Rendering UI Step

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -9,7 +9,7 @@ var ConfirmBattleContainer = React.createClass({
   getInitialState: function () {
     return {
       isLoading: true,
-      playerInfo: [],
+      playersInfo: [],
     };
   },
 
@@ -25,10 +25,20 @@ var ConfirmBattleContainer = React.createClass({
     }.bind(this));
   },
 
+  handleInitiateBattle: function () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo,
+      },
+    });
+  },
+
   render: function () {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo}/>
     );
   },
