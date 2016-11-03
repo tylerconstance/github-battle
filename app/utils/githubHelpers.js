63044895dc5b944a6c7e6e68b4fc3db178d6b4a0
Tyler Constance <tyler@tylerconstance.com>
2016-11-03 17:44:09 -0400

    Added battle scoring

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -4,12 +4,41 @@ var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
-function getUserInfo (username) {
+function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count;
+  });
+}
+
+function getPlayersData(player) {
+  // get Repos
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars,
+      };
+    });
+}
+
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars,
+  ];
+}
+
 var helpers = {
-  getPlayersInfo: function(players) {
+  getPlayersInfo: function (players) {
 
     // Fetch data from Github.
     return axios.all(players.map(function (username) {
@@ -21,6 +50,15 @@ var helpers = {
     }).catch(function (err) {
       console.warn('Error in getPlayersInfo', err);
     });
+  },
+
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn("Error in getPlayersInfo " + err); });
   },
 };
 
